670a736898b472c60a207b87b451f5777b09fa0f
Anita Vincent <paes.anita@gmail.com>
2019-07-17 16:31:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Tag from "@vtex/styleguide/lib/Tag"
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <br/>
+      <Tag>Hi!</Tag>
+    </div>
+  )
+}
+
+export default App
