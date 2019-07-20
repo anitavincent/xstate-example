a108c5afec3db3f6e0d32f916c4ccf68125444c5
Anita Vincent <paes.anita@gmail.com>
2019-07-20 13:30:26 -0300

    Add layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Email from "./components/Email"
 
 function App() {
   return (
-    <div className="App">
-      <Email />
+    <div className="vh-100 bg-light-silver flex flex-column justify-center">
+      <div className="w5 center">
+        <div className="f3 tc mv7">Anita's Fan Club</div>
+        <Email />
+      </div>
     </div>
   )
 }
